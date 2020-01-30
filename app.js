7f078006ece20e3aae44fc4826ad0541491c440e
joao.brasil <TSPD-DT-00331@TSPD-NB-00404.tecnospeed.local>
2020-01-30 08:04:51 -0300

    Ajustando o puppeteer

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const puppeteer = require('puppeteer');
-var CronJob = require('cron').CronJob;
+// const puppeteer = require('puppeteer');
+// var CronJob = require('cron').CronJob;
 
 const express = require('express');
 var cors = require('express-cors');
@@ -14,44 +14,44 @@ app.get('/status', function(req, res) {
 
 app.listen(3001);
 
-const scrape = async () => {
-  // const browser = await puppeteer.launch();
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
-  const page = await browser.newPage();
-  await page.goto('http://books.toscrape.com/');
+// const scrape = async () => {
+//   // const browser = await puppeteer.launch();
+//   const browser = await puppeteer.launch({
+//     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+//   });
+//   const page = await browser.newPage();
+//   await page.goto('http://books.toscrape.com/');
 
-  const result = await page.evaluate(() => {
-    const books = [];
-    document.querySelectorAll('section > div > ol > li').forEach(book =>
-      books.push({
-        prod: book.getElementsByTagName('h3')[0].innerText,
-        price: book.getElementsByClassName('price_color')[0].innerText,
-      })
-    );
-    return books;
-  });
+//   const result = await page.evaluate(() => {
+//     const books = [];
+//     document.querySelectorAll('section > div > ol > li').forEach(book =>
+//       books.push({
+//         prod: book.getElementsByTagName('h3')[0].innerText,
+//         price: book.getElementsByClassName('price_color')[0].innerText,
+//       })
+//     );
+//     return books;
+//   });
 
-  browser.close();
-  return result;
-};
+//   browser.close();
+//   return result;
+// };
 
-var job = new CronJob('* * * * *', () => {
-  console.log('---Iniciando o Monitoramento ---');
+// var job = new CronJob('* * * * *', () => {
+//   console.log('---Iniciando o Monitoramento ---');
 
-  scrape().then(value => {
-    // const prod = value.map(produto => {
-    //   return produto.prod;
-    // });
+//   scrape().then(value => {
+//     // const prod = value.map(produto => {
+//     //   return produto.prod;
+//     // });
 
-    // const price = value.map(preco => {
-    //   return preco.price;
-    // });
+//     // const price = value.map(preco => {
+//     //   return preco.price;
+//     // });
 
-    // console.log('produto', prod);
-    // console.log('preço', price);
-    console.log(value);
-  });
-});
-job.start();
+//     // console.log('produto', prod);
+//     // console.log('preço', price);
+//     console.log(value);
+//   });
+// });
+// job.start();
